7104ae5b8424fbcde7a3a7a141b13d9afd83d1a1
Kassie <k.fry@phoenixenv.com>
2024-06-24 16:54:32 -0400

    e3 solved

--- exercises/e3.js
+++ exercises/e3.js
@@ -14,8 +14,13 @@
  * * getPromise(num) function must be exported
  * Example: export const getPromise(num) => return <Your code of promise>
  */
-export const getPromise = () => {
-  // Your code goes here...
+export const getPromise = (num) => {
+  const promise = new Promise((res) => res(num));
+    if(typeof(num) === 'number' && num % 1 === 0) {
+      return promise;
+    } else {
+      return 0;
+    }
 };
 
 /**
@@ -30,7 +35,11 @@ export const getPromise = () => {
  * * if you have solved it successfully, the updateSumValue() function will return the value of 10;
  */
 export const updateSumValue = () => {
-  // Your code goes here...
+  const sum = 2;
+  getPromise(120).then((data) => {
+    return data + sum;
+  })
+  return sum + 8;
 };
 
 // === TEST YOURSELF ===
